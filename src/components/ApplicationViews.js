824d4a96a4d5cab575d31d2caa16b6819accfdd5
NSS4 <nss4@NSS1s-MacBook-Pro.local>
2019-11-19 11:32:33 -0600

    In-class lecture for chapter 3 and 4

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,27 @@
+import { Route } from 'react-router-dom'
+import React, { Component } from 'react'
+import Home from './home/Home'
+import AnimalList from './animal/AnimalList.js'
+//only include these once they are built - previous practice exercise
+// import LocationCard from './location/LocationCard'
+// import EmployeeCard from './employee/EmployeeCard'
+// import OwnerCard from './owner/OwnerCard'
+
+
+class ApplicationViews extends Component {
+
+  render() {
+    return (
+      <React.Fragment>
+        <Route exact path="/" render={(props) => {
+          return <Home />
+        }} />
+        <Route path="/animals" render={(props) => {
+          return <AnimalList />
+        }} />
+      </React.Fragment>
+    )
+  }
+}
+
+export default ApplicationViews
